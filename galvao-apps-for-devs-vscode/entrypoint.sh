a84ca50dd9acbab6d6591bdb96c105e4e1bd459a
lucas <lucasgalvaonu@gmail.com>
2025-03-23 19:24:22 -0300

    feat: update docker dind network

--- galvao-apps-for-devs-vscode/entrypoint.sh
+++ galvao-apps-for-devs-vscode/entrypoint.sh
@@ -1,36 +1,23 @@
-pidfile="/var/run/docker.pid"
-if [ -f "$pidfile" ]; then
-    rm -f "$pidfile"
-fi
+#!/bin/sh
 
-# Use nftables as the backend for iptables
-for command in iptables iptables-restore iptables-restore-translate iptables-save iptables-translate
-  do
-    ln -sf /sbin/xtables-nft-multi /sbin/$command
-  done
-
-# Ensure that a bridge exists with the given name
 ensure_bridge_exists() {
-    name="$1"
-    ip_range="$2"
+  local bridge="$1"
+  local ip_range="$2"
 
-    if ip link show "$name" >/dev/null 2>&1; then
-        echo "Bridge '$name' already exists. Skipping creation."
-        ip addr show "$name"
-        return
-    fi
-
-    echo "Bridge '$name' does not exist. Creating..."
-    ip link add "$name" type bridge
-    ip addr add "$ip_range" dev "$name"
-    ip link set "$name" up
-    ip addr show "$name"
+  if ! ip link show "$bridge" >/dev/null 2>&1; then
+    echo "Criando bridge $bridge com IP $ip_range"
+    ip link add "$bridge" type bridge
+    ip addr add "$ip_range" dev "$bridge"
+    ip link set "$bridge" up
+  else
+    echo "Bridge $bridge já existe."
+  fi
 }
 
 if [ -n "$DOCKER_ENSURE_BRIDGE" ]; then
-    bridge="${DOCKER_ENSURE_BRIDGE%%:*}"
-    ip_range="${DOCKER_ENSURE_BRIDGE#*:}"
-    ensure_bridge_exists "$bridge" "$ip_range"
+  bridge="${DOCKER_ENSURE_BRIDGE%%:*}"
+  ip_range="${DOCKER_ENSURE_BRIDGE#*:}"
+  ensure_bridge_exists "$bridge" "$ip_range"
 fi
 
-exec dockerd-entrypoint.sh "$@"
+exec "$@"
